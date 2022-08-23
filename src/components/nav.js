89729c2ff8bc67d4252c475b447921a59dbd49f8
morteza pasandi <morteza.mmp1375@gmail.com>
2022-08-22 22:19:04 -0400

    v0.5.2 nav-no-detail

--- src/components/nav.js
+++ src/components/nav.js
@@ -8,40 +8,30 @@ function Nav() {
 	    const [activeTheme, setTheme] = useThemeSwitcher();  
       const [active, setActive] = useState(0);
       return (
-        <div className="  font-[Garamond] justify-items-center
-        bottom-12 w-max  fixed justify-center block  -translate-x-1/2 items-center   left-2/4 px-3 py-4"  >
- <ul className="flex relative">
+        <div className="  font-[Garamond] justify-items-center 
+        bottom-12 max-w-fit     fixed justify-center block  -translate-x-1/2 items-center z-40   left-2/4   "  >
+          <ul className="flex relative rounded-3xl p-1 border-emerald-200 border-2  bg-[rgba(255,255,255,.6)] dark:bg-[rgba(0,0,0,.6)]">
 
             {Content.Nav.Menus.map((menu, i) => (
               <li key={i} className="w-16">
                 <ScrollLink to={menu.link} smooth={true} key={i}>
                 <a className="flex flex-col  " href={menu.link}  onClick={() => setActive(i)} >
                   <span
-                    className={`  justify-center  aspect-square	w-7  flex text-xl p-1 cursor-pointer  z-20 duration-2000 ${
+                    className={`  justify-center  aspect-square	w-7  flex text-xl p-1 cursor-pointer   duration-2000 ${
                       i === active && " text-zinc-900 bg-lime-100 dark:text-zinc-50 dark:bg-lime-600 rounded-full   "
                     } ${i !== active && " text-violet-500   hover:text-violet-500  "}`}
                   >
                     {menu.icon} 
                   </span>
-                  <span
-                    className={` ${
-                      active === i
-                        ? "-translate-y-1 duration-2000 opacity-100  text-black dark:text-white "
-                        : "opacity-0 translate-y-4   "
-                    } `}
-                  >
-                    {menu.name}
-                  </span>
+                  
                 </a>
                 </ScrollLink>
-
-
               </li>
               
             ))}
  
             <div onClick={() => setTheme(activeTheme)} aria-label="Theme Switcher" 
-               className="   text-violet-500  -translate-x-5  text-xl -translate-y-2 hover:text-black hover:dark:text-white  bg-primary-light dark:bg-ternary-dark p-3 shadow-sm rounded-xl cursor-pointer" >
+               className="   text-violet-500   text-xl    translate-y-1 hover:text-black hover:dark:text-white  bg-primary-light dark:bg-ternary-dark shadow-sm rounded-xl cursor-pointer" >
 						{activeTheme === 'dark' ? (
 							 <h1  >{Content.Nav.lighticon[1]} </h1>
 						) : (
@@ -55,13 +45,13 @@ function Nav() {
     };
 export default Nav
 /**
- * 					<Link to="/"><div>
-                  {activeTheme === 'dark' ? (
-                    Content.Nav.lighticon[0] 
-                       
-                  ) : (
-                    Content.Nav.lighticon[1] 
-                  )}
-              </div>
-              </Link>
+ * 	                  <span
+                    className={` ${
+                      active === i
+                        ? " duration-2000 opacity-100  text-black dark:text-white "
+                        : "opacity-0    "
+                    } `}
+                  >
+                    {menu.name}
+                  </span>
  */
